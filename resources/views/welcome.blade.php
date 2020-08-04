40539ad4167bc9ed7e4a57a8792de333ae736990
Kennedy Mutisya <ken.msh@gmail.com>
2020-08-04 11:08:05 +0300

    Update icons and mission on landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,7 +15,7 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-    <link href='https://api.mapbox.com/mapbox-gl-js/v1.11.1/mapbox-gl.css' rel='stylesheet' />
+    <link href='https://api.mapbox.com/mapbox-gl-js/v1.11.1/mapbox-gl.css' rel='stylesheet'/>
     <link
         href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
         rel="stylesheet">
@@ -23,7 +23,7 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-<div>
+<div class="container-fluid">
 
     <div class="row">
         <div class="col-md-8 offset-2 text-center">
@@ -37,46 +37,71 @@
     </div>
 
     <div class="container">
-        <div style="position:relative;width:100%;height: 549px;background: white;border-radius: 30px;margin-bottom:61px"
+        <div style="position:relative;width:100%;height: 549px;background: white;border-radius: 20px;margin-bottom:61px"
              id="map">
             <div class="shadow" style="z-index:100;position: absolute;margin-left:26px;margin-top:36px;height: 160px;
             width:20rem;">
                 <div class="card">
-                  <div class="card-body">
-                      <!--Label: Type, Attributes:type -->
-                      <div class="form-group">
-                          <label for="type">Type</label>
-                          <select type="text" class="form-control form-control-sm" id="type"
-                                 aria-describedby="type-help"
-                                  required>
-                              <option value="type">Type</option>
-                          </select>
+                    <div class="card-body">
+                        <!--Label: Type, Attributes:type -->
+                        <div class="form-group">
+                            <label for="type">Type</label>
+                            <select type="text" class="form-control form-control-sm" id="type"
+                                    aria-describedby="type-help"
+                                    required>
+                                <option value="type">Type</option>
+                            </select>
 
-                      </div>
-                      <div class="form-group">
-                          <label for="type">Type</label>
-                          <select type="text" class="form-control form-control-sm" id="type"
-                                 aria-describedby="type-help"
-                                  required>
-                              <option value="type">Kenya</option>
-                          </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="type">Type</label>
+                            <select type="text" class="form-control form-control-sm" id="type"
+                                    aria-describedby="type-help"
+                                    required>
+                                <option value="type">Kenya</option>
+                            </select>
 
-                      </div>
-                      <div class="form-group">
-                          <label for="type">Type</label>
-                          <select type="text" class="form-control form-control-sm" id="type"
-                                 aria-describedby="type-help"
-                                  required>
-                              <option value="type">Return 10%</option>
-                          </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="type">Type</label>
+                            <select type="text" class="form-control form-control-sm" id="type"
+                                    aria-describedby="type-help"
+                                    required>
+                                <option value="type">Return 10%</option>
+                            </select>
 
-                      </div>
-                  </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+    <div class="container">
+        <div class="row">
 
+        </div>
+        <div class="row" style="margin-top: 74px">
+            <div class="col-12"
+                style="z-index:10;margin-left:-30px;margin-top:-25px;font: Bold 40px Roboto;color: #DBDCDC !important;">
+                Mission
+            </div>
+            <div class="col-12">
+                <p class="text-center" style="color: #DBDCDC;font: Bold 20px/26px Roboto;text-align: center;
+                margin-top: 38px">
+                    Untapped’s mission is to help entrepreneurs in frontier markets solve everyday problems with the
+                    right technology and access to capital.
+                </p>
+            </div>
+            <div class="col-12">
+                <div class="row justify-content-center mt-5">
+                    <img src="{{ asset('untapped/lead.svg') }}" alt="">
+                    <img class="pl-5" src="{{ asset('untapped/dashboard.svg') }}" alt="">
+                    <img class="pl-5" src="{{ asset('untapped/droplet.svg') }}" alt="">
+                    <img class="pl-5" src="{{ asset('untapped/motorbiked.svg') }}" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 <script src='https://api.mapbox.com/mapbox-gl-js/v1.11.1/mapbox-gl.js'></script>
 
