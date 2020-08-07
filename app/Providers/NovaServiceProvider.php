be37b30d97eb29b393d8db145c5cf8b0252c7b3c
Kennedy Mutisya <ken.msh@gmail.com>
2020-08-07 20:29:45 +0300

    Update dashboard

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -57,7 +57,7 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     protected function cards()
     {
         return [
-            new Help,
+//            new Help,
         ];
     }
 
