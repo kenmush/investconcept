8eae31ed3512c9a8370c120401047dbbb97ce20f
Kennedy Mutisya <kenmsh@gmail.com>
2020-10-31 13:24:35 +0300

    Remove call to backend

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ Route::get('test/{investmentid}', function ($investmentId) {
 
 Route::get('/', function () {
     $assets = (new Investor())->getAssetCategories();
-    $landingPageData = collect((new Investor())->getLandingPageData())->first();
+//    $landingPageData = collect((new Investor())->getLandingPageData())->first();
 //    $landingPageData = [];
-    return view('index', compact('assets','landingPageData'));
+    return view('index', compact('assets'));
 });
 
 Route::group(['prefix' => 'assets', 'middleware' => ['auth']], function () {
