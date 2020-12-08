cf10fe2780d2309d78e4d0af4ab9e65460de1d00
Kennedy Mutisya <kenmutisya@mmttc.ac.ke>
2020-12-08 22:24:03 +0300

    update css and js

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,6 +3,9 @@ const mix = require('laravel-mix');
 require('laravel-mix-purgecss');
 
 mix.js('resources/js/app.js', 'public/js')
+    .extract([
+        'vue','axios','popper.js','jquery','bootstrap',
+    ])
     .browserSync('localhost:8000')
     .sass('resources/sass/app.scss', 'public/css')
     .sass('resources/sass/front.scss', 'public/css')
