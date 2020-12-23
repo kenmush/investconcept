4d9021ce1236b3494293f36c343155fb0d806aa2
Kennedy Mutisya <kenmutisya@mmttc.ac.ke>
2020-12-23 17:35:55 +0300

    Update ui

--- app/Http/Controllers/IrrigationPumps.php
+++ app/Http/Controllers/IrrigationPumps.php
@@ -48,22 +48,47 @@ class IrrigationPumps extends Controller
                 $benefiaries['founded'] = '2019';
                 $benefiaries['Assets managed'] = 1;
                 $benefiaries['Planned Q1 2021'] = 5;
+
                 $benefiaries['Mission'] =
                         "To make high quality drinking water available on demand at scale to the Kenyan urban middle class";
             }
-            if ($benefiaries['firstName'] === 'OVIVE purified') {
+            if ($benefiaries['firstName'] === 'Ovive Purified') {
                 $benefiaries['founded'] = '2019';
                 $benefiaries['Assets managed'] = 1;
                 $benefiaries['Planned Q1 2021'] = 5;
                 $benefiaries['Mission'] =
                         "To make high quality drinking water available on demand at scale to the Kenyan urban middle class.";
             }
-            if ($benefiaries['firstName'] === 'Maji') {
+            if ($benefiaries['firstName'] === 'Maji Milele') {
                 $benefiaries['founded'] = '2008';
                 $benefiaries['Assets managed'] = 9;
                 $benefiaries['Planned Q1 2021'] = 25;
                 $benefiaries['Mission'] =
                         "Widening base of pyramid access to safe water";
+            }
+            if ($benefiaries['firstName'] === 'Asaak') {
+                $benefiaries['founded'] = '';
+                $benefiaries['Assets managed'] = 40;
+                $benefiaries['Planned Q1 2021'] = round(0.4 * 40);
+                $benefiaries['Mission'] =
+                        "Asaak is on a mission to democratize financial services by helping businesses
+grow with confidence
+
+";
+            }
+            if ($benefiaries['firstName'] === 'Zembo') {
+                $benefiaries['founded'] = '';
+                $benefiaries['Assets managed'] = 83;
+                $benefiaries['Planned Q1 2021'] = round(0.4 * 83);
+                $benefiaries['Mission'] =
+                        "Zembo, the Zero Emission Motorcycle Boda";
+            }
+            if ($benefiaries['firstName'] === 'Teliman') {
+                $benefiaries['founded'] = '';
+                $benefiaries['Assets managed'] = 15;
+                $benefiaries['Planned Q1 2021'] = round(0.4 * 15);
+                $benefiaries['Mission'] =
+                        "";
             }
             return $benefiaries;
         });
--- app/Http/Controllers/MotoTaxisController.php
+++ app/Http/Controllers/MotoTaxisController.php
@@ -48,17 +48,18 @@ class MotoTaxisController extends Controller
                 $benefiaries['founded'] = '2019';
                 $benefiaries['Assets managed'] = 1;
                 $benefiaries['Planned Q1 2021'] = 5;
+
                 $benefiaries['Mission'] =
                         "To make high quality drinking water available on demand at scale to the Kenyan urban middle class";
             }
-            if ($benefiaries['firstName'] === 'Ovive Purified Water ATM') {
+            if ($benefiaries['firstName'] === 'Ovive Purified') {
                 $benefiaries['founded'] = '2019';
                 $benefiaries['Assets managed'] = 1;
                 $benefiaries['Planned Q1 2021'] = 5;
                 $benefiaries['Mission'] =
                         "To make high quality drinking water available on demand at scale to the Kenyan urban middle class.";
             }
-            if ($benefiaries['firstName'] === 'Maji') {
+            if ($benefiaries['firstName'] === 'Maji Milele') {
                 $benefiaries['founded'] = '2008';
                 $benefiaries['Assets managed'] = 9;
                 $benefiaries['Planned Q1 2021'] = 25;
