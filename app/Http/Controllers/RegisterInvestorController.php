aa999a1da0afaba56f9a8e1b9d2afa9f1531dfbf
Kennedy Mutisya <kenmutisya@mmttc.ac.ke>
2021-01-25 22:33:58 +0300

    refactor: Add RegisterInvestorController

--- app/Http/Controllers/RegisterInvestorController.php
+++ app/Http/Controllers/RegisterInvestorController.php
@@ -2,10 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\UploadInvestorDocuments;
+use App\Services\Investor;
+use GuzzleHttp\Exception\ClientException;
+use Illuminate\Http\Request;
+
 class RegisterInvestorController extends Controller
 {
-    public function __invoke()
+    public function __invoke(Request $request)
     {
-        //
+        try {
+            $investor = (new Investor())->registerInvestor([
+                    'phoneNumber'  => 'NA',
+                    'firstName'    => $request->firstname,
+                    'middleName'   => '',
+                    'lastName'     => $request->lastname,
+                    'username'     => $request->investoremail,
+                    'language'     => 'English',
+                    'email'        => $request->investoremail,
+                    'organization' => 'NA',
+                    'password'     => $request->investorpassword,
+                    'avatar'       => null,
+            ]);
+
+            if ($request->hasFile('passport')) {
+                $document = $request->file('passport')->store('documents', 'public');
+            }
+            if ($request->hasFile('w9form')) {
+                $w9Form = $request->file('w9form')->store('w9s', 'public');
+            }
+            UploadInvestorDocuments::dispatch($investor, $request->except('passport', 'w9form'), $document, $w9Form);
+        } catch (ClientException $e) {
+            $errors = collect(json_decode($e->getResponse()->getBody()->getContents()));
+
+            return response()->json($errors, 422);
+        }
     }
 }
