e17565648d458c667dbaa43dd3f87f6b3538dd11
Kennedy Mutisya <kenmutisya@mmttc.ac.ke>
2021-01-25 22:33:09 +0300

    refactor: Refactor route cloure to Controller

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,6 @@
 <?php
 
 use App\Asset;
-use App\Events\UploadInvestorDocuments;
-use App\Http\Controllers\AllassetController;
-use App\Http\Controllers\ChangePasswordController;
-use App\Http\Controllers\MotoTaxisController;
-use App\Http\Controllers\QuestinnareController;
-use App\Services\Investor;
-use GuzzleHttp\Exception\ClientException;
-use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'WelcomePageController');
 
@@ -36,7 +28,7 @@ Route::group(['prefix' => 'assets', 'middleware' => ['auth', 'changepassword']],
 
 Route::group(['prefix' => 'administrate', 'middleware' => ['auth']], function () {
     Route::resource('herosection', 'LandingpageController');
-    Route::get('/manageasset/{id?}/{investor?}', [AllassetController::class, 'showforUse']);
+    Route::get('/manageasset/{id?}/{investor?}', 'AllassetController@showforUse');
     Route::resource('investors', 'Investors');
     Route::resource('investors', 'Investors');
 
@@ -68,34 +60,7 @@ Route::get('irrigation-pumps', 'IrrigationPumps');
 Route::get('questionnaire', 'QuestinnareController')->name('questionarre');
 Route::post('questionnaire', 'UploadQuestionaire')->name('uploadquestionaire');
 
-Route::post('registeraninvestor', function (Illuminate\Http\Request $request) {
-    try {
-        $investor = (new Investor())->registerInvestor([
-                'phoneNumber'  => 'NA',
-                'firstName'    => $request->firstname,
-                'middleName'   => '',
-                'lastName'     => $request->lastname,
-                'username'     => $request->investoremail,
-                'language'     => 'English',
-                'email'        => $request->investoremail,
-                'organization' => 'NA',
-                'password'     => $request->investorpassword,
-                'avatar'       => null,
-        ]);
-
-        if ($request->hasFile('passport')) {
-            $document = $request->file('passport')->store('documents', 'public');
-        }
-        if ($request->hasFile('w9form')) {
-            $w9Form = $request->file('w9form')->store('w9s', 'public');
-        }
-        UploadInvestorDocuments::dispatch($investor, $request->except('passport', 'w9form'), $document, $w9Form);
-    } catch (ClientException $e) {
-        $errors = collect(json_decode($e->getResponse()->getBody()->getContents()));
-
-        return response()->json($errors, 422);
-    }
-})->name('registeraninvestor');
+Route::post('registeraninvestor', 'RegisterInvestorController')->name('registeraninvestor');
 
 Route::get('successfully/unsubscribed', function () {
     return redirect('/');
