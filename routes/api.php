834e5ecb1d93f284b8c565b7bb6da622d1cfe931
Kennedy Mutisya <kenmutisya@mmttc.ac.ke>
2021-01-30 17:09:11 +0300

    fix: Cache API call to get investor assets

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,10 @@ use App\Services\Investor;
 use Illuminate\Support\Facades\Route;
 
 Route::get('allAssets', function () {
-    return (new Investor())->getAllAssets();
+    $assets = Cache::remember('assets',18500, function () {
+        return (new Investor())->getAllAssets();
+    });
+    return $assets;
 });
 
 Route::get('getAllAssets/{categoryId?}/{investorid?}', function ($categoryId = 1, $investorid = 1) {
