0131d14bafda4beb20dd575808b9b56c71fab329
Kennedy Mutisya <kenmutisya@mmttc.ac.ke>
2021-02-02 21:56:08 +0300

    feat: Add engage subdomain

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,11 @@
 use App\Asset;
 use Symfony\Component\Process\Process;
 
+Route::domain('engage.untapped-global.com', function () {
+    Route::get('/', function () {
+        return view('engage');
+    });
+});
 Route::get('/', 'WelcomePageController');
 
 Route::group(['prefix' => 'assets', 'middleware' => ['auth', 'changepassword']], function () {
