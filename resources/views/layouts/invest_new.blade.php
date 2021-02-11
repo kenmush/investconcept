1c56258f4f9379e3ae304f416482a51a95f5de4f
Kennedy Mutisya <kenmsh@gmail.com>
2021-02-12 00:41:07 +0300

    feat: Add new Header and footer for the invest page

---
+++ resources/views/layouts/invest_new.blade.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon.ico">
+
+    <title>New Invest Page</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/untapped.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+<div id="app">
+    <div id="header" class="header">
+        <div class="top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-6 col-lg-6">
+                        <div class="welcome-text"><p>Welcome to Untapped</p></div>
+                    </div>
+                    <div class="col-xl-6 col-lg-6 d-xl-flex d-lg-flex d-block align-items-center">
+                        <div class="user-panel"><span><a href="#" class="login">Login</a>or</span><a href="#"
+                                                                                                     class="btn-hyipox-small">Register</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="bottom">
+            <div class="container-fluid">
+                <div class="row justify-content-between">
+                    <div class="col-xl-4 col-lg-2 d-xl-flex d-lg-flex d-block align-items-center">
+                        <div class="row">
+                            <div class="col-6 d-xl-none d-lg-none d-block">
+                                <button type="button" class="navbar-toggler"><span class="dag"></span> <span
+                                            class="dag2"></span> <span class="dag3"></span></button>
+                            </div>
+                            <div class="col-xl-12 col-lg-12 col-6 d-xl-block d-lg-block d-flex align-items-center justify-content-end">
+                                <div class="logo"><a href="/"><img
+                                                src="https://untapped-global.com/wp-content/themes/yootheme/cache/untapped-logo-website1-623de87f.png"
+                                                alt=""></a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-8 col-lg-10">
+                        <div class="mainmenu">
+                            <div class="d-xl-none d-lg-none d-block"></div>
+                            <nav class="navbar navbar-expand-lg">
+                                <div id="navbarSupportedContent" class="collapse navbar-collapse">
+                                    <ul class="navbar-nav ml-auto">
+                                        <li class="nav-item invest-btn text-center">
+                                            <a href="" class="nav-link ">
+                                                INVEST
+                                                <span class="sr-only">(current)</span>
+                                            </a>
+                                        </li>
+                                        <li class="nav-item engage-btn ml-2 text-center">
+                                            <a href="#assets" class="nav-link">
+                                                ENGAGE
+                                                <span class="sr-only">(current)</span>
+                                            </a>
+                                        </li>
+                                        <li class="nav-item untapped-nav-divider">
+                                            <a href="#simulateimpact" class="nav-link ">Home
+                                                <span class="sr-only">(current)</span>
+                                            </a>
+                                        </li>
+                                        <li class="nav-item untapped-nav-divider">
+                                            <a href="/login" class="nav-link">
+                                                About
+                                            </a>
+                                        </li>
+                                        <li class="nav-item untapped-nav-divider">
+                                            <a href="/login" class="nav-link">
+                                                <i class="fa fa-lock"></i>
+                                                How it Works
+                                            </a>
+                                        </li>
+                                        <li class="nav-item join-now-btn untapped-nav-divider">
+                                            <a href="/register" class="nav-link">Blog</a>
+                                        </li>
+                                        <li class="nav-item join-now-btn untapped-nav-divider">
+                                            <a href="/register" class="nav-link">Contact</a>
+                                        </li>
+                                        <li class="nav-item btn login-button">
+                                            <a href="/register" class="nav-link">Log In</a>
+                                        </li>
+                                        <li class="nav-item signup-button">
+                                            <a href="/register" class="nav-link">Sign Up</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="cta">
+        <div class="container mb-5">
+            <div class="cta-bg">
+
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="d-flex justify-content-center align-items-center">
+                    <a href="" class="cta-link">
+                        <div class="cta-invest d-flex justify-content-center align-items-end mb-0 pb-0"
+                             style="position:relative;">
+                            <div style="position: absolute;top: -1rem;z-index: 99">
+                                <svg height="48" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 70.6 90.83">
+                                    <g data-name="Layer 2">
+                                        <g
+                                                data-name="Layer 1">
+                                            <path d="M69.71 57.69L55.35 30.63 41 3.58a6.32 6.32 0 00-11.39 0L15.25 30.63.89 57.69c-2.53 4.77.63 10.73 5.7 10.73H64c5.08 0 8.24-5.96 5.71-10.73zM35.3 41.75H22.21l6.55-12.92 6.54-12.91 6.54 12.91 6.55 12.92z"
+                                                  fill="#f2f2f2"/>
+                                            <path d="M53.69 90.83H14.84c-1.84 0-3.32-1.14-3.32-2.55s1.48-2.55 3.32-2.55h38.85c1.84 0 3.32 1.14 3.32 2.55s-1.48 2.55-3.32 2.55z"
+                                                  fill="#b8dae9"/>
+                                        </g>
+                                    </g>
+                                </svg>
+                            </div>
+
+                            <p>INVEST NOW!</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-12">
+                <div class="d-flex justify-content-center align-items-center">
+                    <h3 class="untapped-title">Your Impact in real time</h3>
+                </div>
+                <div class="d-flex justify-content-center align-items-center">
+
+                    <div style="border-bottom: 6px solid #B8DAE9;width: 20%">
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <div class="container-fluid m-0 p-0">
+        <div style="width: 100%;margin-right: 100px;height:900px !important;">
+            <googlemap></googlemap>
+            {{--            <map-component :categories='@json($assets)'></map-component>--}}
+        </div>
+    </div>
+    <div class="container m-0 p-0">
+        <showassets></showassets>
+    </div>
+    <div class="container-fluid m-0 p-0">
+        <div class="abovefooter"></div>
+
+    </div>
+    <div class="container-fluid" style="background-color: #CCCCCC;color: black">
+
+        <footer>
+            <div class="row">
+                <div class="col-md-4 d-flex justify-center">
+                    <img src="./assets/img/footericon.png" alt="">
+                </div>
+                <div class="col-md-2">
+                    <p>USA</p>
+                    <p>353 Sacramento Str.</p>
+                    <p>Suite 1800</p>
+                    <p>San Francisco | CA 94111</p>
+                </div>
+                <div class="col-md-2">
+                    <p>Kenya</p>
+                    <p>iHub</p>
+                    <p>Senteu Plaza | Kilimani</p>
+                    <p>Nairobi</p>
+                </div>
+                <div class="col-md-2">
+                    <p>Mali</p>
+                    <p>Hamdallay AC200</p>
+                    <p>Immeuble ABK2 | #102</p>
+                    <p>Bamako</p>
+                </div>
+                <div class="col-md-2">
+                    <p>South Africa</p>
+                    <p>Workshop 17</p>
+                    <p>32 Kloof Street</p>
+                    <p>Cape Town | 8001</p>
+                </div>
+            </div>
+        </footer>
+    </div>
+
+</div>
+<script src="{{ mix('js/manifest.js') }}"></script>
+<script src="{{ mix('js/vendor.js') }}"></script>
+<script src="{{ mix('js/app.js') }}"></script>
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+
+<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+<script src="https://maxcdn.bootstrapcdn.com/js/ie10-viewport-bug-workaround.js"></script>
+<!-- Holder.js for placeholder images -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/holder/2.9.4/holder.min.js"></script>
+</body>
+</html>
