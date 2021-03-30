c8982c5d84424eda70986b1db2c5e1102afbbee6
Kennedy Mutisya <kenmsh@gmail.com>
2021-03-30 23:51:56 +0300

    fix: Add contact us link

--- resources/views/layouts/newinvest.blade.php
+++ resources/views/layouts/newinvest.blade.php
@@ -88,7 +88,7 @@
                                class="nav-link">Blog</a>
                         </li>
                         <li class="nav-item">
-                            <a href="#" class="nav-link">Contact Us</a>
+                            <a href="https://untapped-global.com/contact-2/" class="nav-link">Contact Us</a>
                         </li>
                     </ul>
                     <div class="side-nav" style="display:flex">
