dc7d4c88d2e079a80ed21fd74930ba57f7cbc85d
Kennedy Mutisya <kenmsh@gmail.com>
2021-04-09 20:50:01 +0300

    fix: Redirect landing page to registration page

--- app/Http/Controllers/WelcomePageController.php
+++ app/Http/Controllers/WelcomePageController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Services\Investor;
+use function redirect;
 
 class WelcomePageController extends Controller
 {
     public function __invoke()
     {
+        return redirect()->to('register');
         return view('layouts.newinvest');
     }
 }
